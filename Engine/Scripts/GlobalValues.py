b6b36fa9422d3d4026c030fa57edae95b239a0b7
brurmonemt <brurmonemt@protonmail.com>
2025-04-10 20:30:37 -0500

    new big thang

--- Engine/Scripts/GlobalValues.py
+++
@@ -1,8 +0,0 @@
-import pathlib as Dir
-
-class GlobalValues:
-    class Paths:
-        Scripts = Dir.Path().resolve().parent
-        Engine = Scripts.parent
-        Main = Engine.parent
-        Data = Main / "Data"
